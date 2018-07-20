ff85fb11329103f725e4111935afb98241c385ae
threelittlemonkeys <sjlee7642@gmail.com>
2018-07-20 11:06:39 +0900

    Update by commit

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def load_data():
         batch_x.append(tokens[:seq_len] + [EOS_IDX] + pad)
         batch_y.append(tokens[seq_len:] + [EOS_IDX] + pad)
         if len(batch_x) == BATCH_SIZE:
-            data.append((Var(LongTensor(batch_x)), LongTensor(batch_y))) # append a mini-batch
+            data.append((LongTensor(batch_x), LongTensor(batch_y))) # append a mini-batch
             batch_x = []
             batch_y = []
     fo.close()
