4352b49a68b8c1fcb7803f9b44c0f428241d12c0
threelittlemonkeys <sjlee7642@gmail.com>
2018-11-19 16:43:21 +0900

    Update by commit

--- sequence-labelling/prepare.py
+++ sequence-labelling/prepare.py
@@ -1,15 +1,18 @@
 import sys
-import re
-from model import SOS, EOS, PAD, UNK, SOS_IDX, EOS_IDX, PAD_IDX, UNK_IDX
-from utils import normalize
+from utils import *
 
 MIN_LEN = 2
 MAX_LEN = 50
+KEEP_IDX = False
 
 def load_data():
     data = []
-    word_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
-    tag_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX}
+    if KEEP_IDX:
+        word_to_idx = load_word_to_idx(sys.argv[1] + ".word_to_idx")
+        tag_to_idx = load_tag_to_idx(sys.argv[1] + ".tag_to_idx")
+    else:
+        word_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
+        tag_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX}
     fo = open(sys.argv[1])
     for line in fo:
         line = line.strip()
@@ -21,40 +24,24 @@ def load_data():
         for tkn in tokens:
             word, tag = re.split("/(?=[^/]+$)", tkn)
             word = normalize(word)
-            for c in word:
-                if c not in word_to_idx:
-                    word_to_idx[c] = len(word_to_idx)
-            if tag not in tag_to_idx:
-                tag_to_idx[tag] = len(tag_to_idx)
-            seq.extend([str(word_to_idx[c]) for c in word])
+            if not KEEP_IDX:
+                for c in word:
+                    if c not in word_to_idx:
+                        word_to_idx[c] = len(word_to_idx)
+                if tag not in tag_to_idx:
+                    tag_to_idx[tag] = len(tag_to_idx)
+            seq.extend([str(word_to_idx[c]) if c in word_to_idx else str(UNK_IX) for c in word])
             tags.extend([str(tag_to_idx[tag])] * len(word))
         data.append(seq + tags)
     data.sort(key = len, reverse = True)
     fo.close()
     return data, word_to_idx, tag_to_idx
 
-def save_data(data):
-    fo = open(sys.argv[1] + ".csv", "w")
-    for seq in data:
-        fo.write(" ".join(seq) + "\n")
-    fo.close()
-
-def save_word_to_idx(word_to_idx):
-    fo = open(sys.argv[1] + ".word_to_idx", "w")
-    for word, _ in sorted(word_to_idx.items(), key = lambda x: x[1]):
-        fo.write("%s\n" % word)
-    fo.close()
-
-def save_tag_to_idx(tag_to_idx):
-    fo = open(sys.argv[1] + ".tag_to_idx", "w")
-    for tag, _ in sorted(tag_to_idx.items(), key = lambda x: x[1]):
-        fo.write("%s\n" % tag)
-    fo.close()
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: %s training_data" % sys.argv[0])
     data, word_to_idx, tag_to_idx = load_data()
-    save_data(data)
-    save_word_to_idx(word_to_idx)
-    save_tag_to_idx(tag_to_idx)
+    save_data(sys.argv[1], data)
+    if not KEEP_IDX:
+        save_word_to_idx(sys.argv[1], word_to_idx)
+        save_tag_to_idx(sys.argv[1], tag_to_idx)
