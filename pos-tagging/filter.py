aa539b4a40b6eb6fcce4fcbd6711d552d514c407
threelittlemonkeys <sjlee7642@gmail.com>
2019-02-16 10:30:04 +0900

    Update by commit

--- pos-tagging/filter.py
+++ pos-tagging/filter.py
@@ -1,24 +1,21 @@
 import sys
 import re
 
-for line in sys.stdin:
+pl = {}
+fo = open("brown.tagged.merged.uniq")
+for line in fo:
     line = line.strip()
     tkn = line.split()
     out = []
     for word, tag in [re.split("/(?=[^/]+$)", x) for x in tkn]:
         if len(sys.argv) == 1:
+            if tag in pl:
+                continue
+            pl[tag] = True
             print(tag)
         elif tag == sys.argv[1]:
+            if (word, tag) in pl:
+                continue
+            pl[word, tag] = True
             print(word, tag)
-        continue
-        if re.search("'s", word):
-            if tag.count("+") == 2:
-                tags = tag.split("+")
-                print(word[:-2], tags[0], end = " ")
-                print(word[-2:], tags[1])
-            else:
-                if tag[-1] != "$":
-                    print(word, tag)
-                else:
-                    print(word[:-2], tag[:-1], end = " ")
-                    print(word[-2:], "POS")
+fo.close()
