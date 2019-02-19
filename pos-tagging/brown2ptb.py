de983af9dbd2912b56f4cc4c1fb6c58f587b8c15
threelittlemonkeys <sjlee7642@gmail.com>
2019-02-20 01:42:13 +0900

    Update by commit

--- pos-tagging/brown2ptb.py
+++ pos-tagging/brown2ptb.py
@@ -1,18 +1,14 @@
 import sys
 import re
 
-fin = open("brown.tagged.merged.uniq", "r")
-fout = open("brown.tagged.merged.uniq.ptb", "w")
-for line in fin:
-    line = line.strip()
-    tkn = line.split()
+def convert(tkn):
     out = []
-    for word, tag in [re.split("/(?=[^/]+$)", x) for x in tkn]:
+    for word, tag in tkn:
         tags = []
         for x in tag.split("+"):
             neg = False
             pos = False
-            if x[:3] == "FW-":
+            if x[:3] == "FW-": # foreign word
                 tags = ["FW"]
                 break
             if x[-3:] == "-NC": x = x[:-3]
@@ -21,48 +17,64 @@ for line in fin:
             if len(x) and x[-1] == "*":
                 x = x[:-1]
                 neg = True
-            if x == "PP$": tags.append("DT")
-            if x == "WP$": tags.append("DT")
-            if x == "PP$$": tags.append("PN")
             if len(x) and x[-1] == "$":
-                x = x[:-1]
-                pos = True
+                if x == "PP$": tags.append("DT") # possessive pronoun
+                elif x == "PP$$": tags.append("PRO") # possessive pronoun
+                elif x == "WP$": tags.append("WH") # possessive wh-pronoun
+                else:
+                    x = x[:-1]
+                    pos = True
             if re.match("[^A-Z]+$", x): tags.append(x) # other special characters
             if re.match("A(B[LNX]|[PT])$", x): tags.append("DT")
             if x == "BE": tags.append("VB")
-            if re.match("BEDZ?$", x): tags.append("VBD")
-            if re.match("BE[GNZ]$", x): tags.append("VB" + x[-1])
-            if re.match("BE[MR]$", x): tags.append("VBP")
+            if re.match("BEDZ?$", x): tags.append("VB")
+            if re.match("BE[GN]$", x): tags.append("VB")
+            if re.match("BE[MRZ]$", x): tags.append("VB")
             if x == "CC": tags.append("CC")
-            if x == "CS": tags.append("CS")
-            if x == "CD": tags.append("CD")
-            if x == "OD": tags.append("JJ")
+            if x == "CS": tags.append("CC")
+            if x == "CD": tags.append("CD") # cardinal numeral
+            if x == "OD": tags.append("JJ") # ordinal numeral
             if re.match("DO[DZ]?$", x): tags.append("VB")
             if re.match("DT[ISX]?$", x): tags.append("DT")
             if x == "EX": tags.append("RB")
             if x == "HV": tags.append("VB")
-            if re.match("HV[DGNZ]$", x): tags.append("VB" + x[-1])
+            if re.match("HV[DGNZ]$", x): tags.append("VB")
             if x == "IN": tags.append("IN")
             if re.match("JJ[RST]?$", x): tags.append("JJ")
-            if x == "MD": tags.append("MD")
-            if x == "NIL": tags.append("X")
-            if re.match("N([NPR]S?)$", x): tags.append("NN")
-            if re.match("P(N|P[LOS]S?)$", x): tags.append("PN")
+            if x == "MD": tags.append("AUX")
+            if x == "NIL": tags.append("UNK")
+            if re.match("NNS?$", x): tags.append("NN") # noun
+            if re.match("NPS?$", x): tags.append("NN") # proper noun
+            if re.match("NRS?$", x): tags.append("NN") # adverbial noun
+            if re.match("P(N|P[LOS]S?)$", x): tags.append("PRO")
             if re.match("QLP?$", x): tags.append("RB")
             if re.match("RB[RT]?$", x): tags.append("RB")
-            if x == "RN": tags.append("RB")
-            if x == "RP": tags.append("RP")
+            if x == "RN": tags.append("RB") # nominal adverb
+            if x == "RP": tags.append("RP") # particle
             if x == "TO": tags.append("RP")
-            if x == "UH": tags.append("UH")
-            if re.match("VB[DGNZ]?$", x): tags.append(x)
-            if x == "WDT": tags.append("DT")
-            if re.match("WP[OS]?$", x): tags.append("PN")
-            if re.match("W(QL|RB)$", x): tags.append("RB")
+            if x == "UH": tags.append("UH") # interjection
+            if re.match("VB[DGNZ]?$", x): tags.append("VB")
+            if x == "WDT": tags.append("WH")
+            if re.match("WP[OS]$", x): tags.append("WH")
+            if re.match("W(QL|RB)$", x): tags.append("WH")
             if neg:
                 tags.append("NEG")
             if pos:
                 tags.append("POS")
         out.append(word + "/" + "+".join(tags))
-    fout.write(" ".join(out) + "\n")
-fin.close()
-fout.close()
+    return out
+
+def postprocess(tkn):
+    return tkn
+
+if __name__ == "__main__":
+    fin = open("brown.tagged.merged.uniq", "r")
+    fout = open("brown.tagged.merged.uniq.ptb", "w")
+    for line in fin:
+        line = line.strip()
+        tkn = [re.split("/(?=[^/]+$)", x) for x in line.split()]
+        tkn = convert(tkn)
+        tkn = postprocess(tkn)
+        fout.write(" ".join(tkn) + "\n")
+    fin.close()
+    fout.close()
--- pos-tagging/filter.py
+++ pos-tagging/filter.py
@@ -2,27 +2,33 @@ import sys
 import re
 
 pl = {}
-# fo = open("brown.tagged.merged.uniq")
-fo = open("brown.tagged.merged.uniq.ptb")
+
+if sys.argv[1] == "brown":
+    fo = open("brown.tagged.merged.uniq")
+if sys.argv[1] == "ptb":
+    fo = open("brown.tagged.merged.uniq.ptb")
+
 for line in fo:
     line = line.strip()
     tkn = [re.split("/(?=[^/]+$)", x) for x in line.split()]
     for i, (word, tag) in enumerate(tkn):
         word = word.lower()
         # tag = tag.upper()
-        if len(sys.argv) == 1:
+        if len(sys.argv) == 2:
             if tag in pl:
                 continue
             pl[tag] = True
             print(tag)
-        elif word == sys.argv[1] or tag == sys.argv[1]:
+        elif word == sys.argv[2] or tag == sys.argv[2]:
             if (word, tag) in pl:
                 continue
             pl[word, tag] = True
             print(word, tag)
-        elif sys.argv[1] == word + "/" + tag:
+        elif sys.argv[2] == word + "/" + tag:
             out = tkn[max(0, i - 2):i]
             out += [tkn[i]]
             out += tkn[i + 1:min(len(tkn), i + 3)]
             print(" ".join(["/".join(x) for x in out]))
 fo.close()
+
+print(len(pl))
