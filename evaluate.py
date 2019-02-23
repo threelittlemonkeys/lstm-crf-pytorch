ec46b13a5314ae2c54d845684617e6562c5fbc53
threelittlemonkeys <sjlee7642@gmail.com>
2019-02-23 12:07:28 +0900

    Update by commit

--- evaluate.py
+++ evaluate.py
@@ -3,28 +3,28 @@ from collections import defaultdict
 
 def evaluate(result):
     a = defaultdict(float) # average
-    s = defaultdict(int) # entire set
-    p = defaultdict(int) # positive
-    t = defaultdict(int) # true positive
-    for _, y0, y1 in result: # actual value, predicted outcome
+    tp = defaultdict(int) # true positives
+    tpfn = defaultdict(int) # true positives + false negatives
+    tpfp = defaultdict(int) # true positives + false positives
+    for _, y0, y1 in result: # actual value, prediction
         for y0, y1 in zip(y0, y1):
-            s[y0] += 1
-            p[y1] += 1
-            if y0 == y1:
-                t[y0] += 1
-    for y in sorted(s.keys()):
-        pr = t[y] / p[y] if p[y] else 0
-        rc = t[y] / s[y]
+            tp[y0] += y0 == y1
+            tpfn[y0] += 1
+            tpfp[y1] += 1
+    for y in sorted(tpfn.keys()):
+        pr = tp[y] / tpfp[y] if tpfp[y] else 0
+        rc = tp[y] / tpfn[y] if tpfn[y] else 0
         a["macro_pr"] += pr
         a["macro_rc"] += rc
-        print("\nlabel = %s" % y)
-        print("precision = %f (%d/%d)" % (pr, t[y], p[y]))
-        print("recall = %f (%d/%d)" % (rc, t[y], s[y]))
+        print()
+        print("label = %s" % y)
+        print("precision = %f (%d/%d)" % (pr, tp[y], tpfp[y]))
+        print("recall = %f (%d/%d)" % (rc, tp[y], tpfn[y]))
         print("f1 = %f" % f1(pr, rc))
-    a["macro_pr"] /= len(s)
-    a["macro_rc"] /= len(s)
-    a["micro_pr"] = sum(t.values()) / sum(p.values())
-    a["micro_rc"] = sum(t.values()) / sum(s.values())
+    a["macro_pr"] /= len(tpfn)
+    a["macro_rc"] /= len(tpfn)
+    a["micro_pr"] = sum(tp.values()) / sum(tpfp.values())
+    a["micro_rc"] = sum(tp.values()) / sum(tpfn.values())
     print()
     print("macro precision = %f" % a["macro_pr"])
     print("macro recall = %f" % a["macro_rc"])
