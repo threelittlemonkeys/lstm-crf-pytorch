6284e626e31f180f245f1709f118c76461f4bb87
threelittlemonkeys <sjlee7642@gmail.com>
2019-02-24 11:55:52 +0900

    Update by commit

--- evaluate.py
+++ evaluate.py
@@ -2,7 +2,7 @@ from predict import *
 from collections import defaultdict
 
 def evaluate(result):
-    a = defaultdict(float) # average
+    avg = defaultdict(float) # average
     tp = defaultdict(int) # true positives
     tpfn = defaultdict(int) # true positives + false negatives
     tpfp = defaultdict(int) # true positives + false positives
@@ -14,25 +14,25 @@ def evaluate(result):
     for y in sorted(tpfn.keys()):
         pr = tp[y] / tpfp[y] if tpfp[y] else 0
         rc = tp[y] / tpfn[y] if tpfn[y] else 0
-        a["macro_pr"] += pr
-        a["macro_rc"] += rc
+        avg["macro_pr"] += pr
+        avg["macro_rc"] += rc
         print()
         print("label = %s" % y)
         print("precision = %f (%d/%d)" % (pr, tp[y], tpfp[y]))
         print("recall = %f (%d/%d)" % (rc, tp[y], tpfn[y]))
         print("f1 = %f" % f1(pr, rc))
-    a["macro_pr"] /= len(tpfn)
-    a["macro_rc"] /= len(tpfn)
-    a["micro_pr"] = sum(tp.values()) / sum(tpfp.values())
-    a["micro_rc"] = sum(tp.values()) / sum(tpfn.values())
+    avg["macro_pr"] /= len(tpfn)
+    avg["macro_rc"] /= len(tpfn)
+    avg["micro_pr"] = sum(tp.values()) / sum(tpfp.values())
+    avg["micro_rc"] = sum(tp.values()) / sum(tpfn.values())
     print()
-    print("macro precision = %f" % a["macro_pr"])
-    print("macro recall = %f" % a["macro_rc"])
-    print("macro f1 = %f" % f1(a["macro_pr"], a["macro_rc"]))
+    print("macro precision = %f" % avg["macro_pr"])
+    print("macro recall = %f" % avg["macro_rc"])
+    print("macro f1 = %f" % f1(avg["macro_pr"], avg["macro_rc"]))
     print()
-    print("micro precision = %f" % a["micro_pr"])
-    print("micro recall = %f" % a["micro_rc"])
-    print("micro f1 = %f" % f1(a["micro_pr"], a["micro_rc"]))
+    print("micro precision = %f" % avg["micro_pr"])
+    print("micro recall = %f" % avg["micro_rc"])
+    print("micro f1 = %f" % f1(avg["micro_pr"], avg["micro_rc"]))
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
