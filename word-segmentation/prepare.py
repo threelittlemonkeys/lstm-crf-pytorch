d7f557f407d26e15553a6bbcfe16a1efe50039da
threelittlemonkeys <sjlee7642@gmail.com>
2019-03-09 09:53:41 +0900

    Update by commit

--- word-segmentation/prepare.py
+++ word-segmentation/prepare.py
@@ -6,10 +6,10 @@ KEEP_IDX = False # use the existing indices
 def load_data():
     data = []
     if KEEP_IDX:
-        word_to_idx = load_tkn_to_idx(sys.argv[1] + ".word_to_idx")
+        char_to_idx = load_tkn_to_idx(sys.argv[1] + ".char_to_idx")
         tag_to_idx = load_tkn_to_idx(sys.argv[1] + ".tag_to_idx")
     else:
-        word_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
+        char_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
         tag_to_idx = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX}
         # IOB tags
         tag_to_idx["B"] = len(tag_to_idx)
@@ -23,21 +23,21 @@ def load_data():
         for word in tokens:
             if not KEEP_IDX:
                 for c in word:
-                    if c not in word_to_idx:
-                        word_to_idx[c] = len(word_to_idx)
+                    if c not in char_to_idx:
+                        char_to_idx[c] = len(char_to_idx)
             ctags = ["B" if i == 0 else "I" for i in range(len(word))]
-            seq.extend([str(word_to_idx[c]) if c in word_to_idx else str(UNK_IDX) for c in word])
+            seq.extend([str(char_to_idx[c]) if c in char_to_idx else str(UNK_IDX) for c in word])
             tags.extend([str(tag_to_idx[t]) for t in ctags])
         data.append(seq + tags)
     data.sort(key = lambda x: -len(x))
     fo.close()
-    return data, word_to_idx, tag_to_idx
+    return data, char_to_idx, tag_to_idx
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: %s training_data" % sys.argv[0])
-    data, word_to_idx, tag_to_idx = load_data()
+    data, char_to_idx, tag_to_idx = load_data()
     save_data(sys.argv[1] + ".csv", data)
     if not KEEP_IDX:
-        save_tkn_to_idx(sys.argv[1] + ".word_to_idx", word_to_idx)
+        save_tkn_to_idx(sys.argv[1] + ".char_to_idx", char_to_idx)
         save_tkn_to_idx(sys.argv[1] + ".tag_to_idx", tag_to_idx)
