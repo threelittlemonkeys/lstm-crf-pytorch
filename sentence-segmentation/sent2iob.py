da8b4bc77d907630f63bd466424c8e1b0748c763
threelittlemonkeys <sjlee7642@gmail.com>
2019-05-06 01:18:16 +0900

    Update by commit

--- sentence-segmentation/sent2iob.py
+++ sentence-segmentation/sent2iob.py
@@ -3,12 +3,12 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: %s training_data" % sys.argv[0])
-    fo = open(sys.argv[1])
-    data = fo.read()
-    data = data.strip()
-    data = data.split("\n\n")
-    for sents in data:
-        sents = [sent.split(" ") for sent in sents.split("\n")]
-        sents = [[sent[0] + "/B"] + [w + "/I" for w in sent[1:]] for sent in sents]
-        print(" ".join([" ".join(sent) for sent in sents]))
-    fo.close()
+    with open(sys.argv[1]) as fo:
+        data = fo.read()
+    with open(sys.argv[1] + ".iob", "w") as fo:
+        data = data.strip()
+        data = data.split("\n\n")
+        for sents in data:
+            sents = [sent.split(" ") for sent in sents.split("\n")]
+            sents = [[sent[0] + "/B"] + [w + "/I" for w in sent[1:]] for sent in sents]
+            fo.write(" ".join([" ".join(sent) for sent in sents]) + "\n")
